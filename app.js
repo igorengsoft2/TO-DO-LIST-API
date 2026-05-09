74321623f3394a63e9662aafc9e41ac48b11d4bf
igorcybersec1 <igorsoaresmain@gmail.com>
2026-05-09 13:29:00 -0300

    use all routers

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes/user');
+
+const userRouter = require('./routes/user');
+const taskRouter = require('./routes/task');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(routes);
+
+app.use('/users', userRouter);
+app.use('/tasks', taskRouter);
 
 module.exports = app;
