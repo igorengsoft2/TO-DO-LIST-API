0767060e197cf553af451399300b277adfe4d082
igorcybersec1 <igorsoaresmain@gmail.com>
2026-05-09 14:15:59 -0300

    create https server

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8800;
+const https = require('https');
+const fs = require('fs');
+const app = require('./app');
 
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT;
 
-    console.log(`Servidor rodando na porta ${PORT}`);
-})
+const options = {
 
+    key: fs.readFileSync(process.env.KEY),
+    cert: fs.readFileSync(process.env.CERT)
+
+}
+
+https.createServer(options, app).listen(PORT, () => {
+
+    console.log(`Servidor HTTPS rodando na porta ${PORT}`);
+})    
