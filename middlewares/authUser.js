b4658e09dfbce224da780bb9cfb019a44f2a9781
igorcybersec1 <igorsoaresmain@gmail.com>
2026-05-13 01:18:22 -0300

    add user verification

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -1,19 +1,36 @@
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-function authToken(req, res, next) {
+const jwt = require('jsonwebtoken');
+const pool = require('../db');
+
+async function authToken(req, res, next) {
 
     const authHeader = req.headers['authorization'];
     const token = authHeader?.split(' ')[1];
 
     if (!token) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    try {
 
-        if(err) return res.sendStatus(401);
-        req.user = user;
+         const user = jwt.verify(token, process.env.JWT_SECRET);
+         const query = "SELECT nome, id FROM Usuario WHERE id = ?";
+         const values = [user.id];
+         const [rows] = await pool.execute(query, values);          
+
+        if(rows.length === 0) {
+
+            return res.sendStatus(401);
+        }
+        
+        req.user = rows[0];
         next();
-    })
+    }
+
+    catch(e) {
+
+        console.log(e);
+        return res.sendStatus(403);
+    }
 }
 
 module.exports = authToken;
