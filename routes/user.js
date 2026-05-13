2ff125da028a33a49cc7e473484c527ba876eddd
igorcybersec1 <igorsoaresmain@gmail.com>
2026-05-13 01:19:09 -0300

    add get users routes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userRouter = express.Router();
-const loginToken = require('../controllers/users');
+
+const { loginToken, getUsers } = require('../controllers/users');
+const authUser = require('../middlewares/authUser');
 
 userRouter.post('/login', loginToken);
+userRouter.get('/all', authUser, getUsers);
 
 module.exports = userRouter;
